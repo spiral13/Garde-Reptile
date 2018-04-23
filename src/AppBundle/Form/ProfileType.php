2bc9e6d751466ce6a122d68d898f61e8e1e82b35
LiliDevMart <lilidevmart@gmail.com>
2018-04-23 23:31:39 +0200

    Ajout champs Ville dans profile edit

--- src/AppBundle/Form/ProfileType.php
+++ src/AppBundle/Form/ProfileType.php
@@ -1,25 +1,31 @@
 <?php
-// src/AppBundle/Form/RegistrationType.php
+// src/AppBundle/Form/ProfileType.php
 
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class RegistrationType extends AbstractType
+class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->add('ville');
     }
 
     public function getParent()
     {
-        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
     }
 
     public function getBlockPrefix()
     {
-        return 'app_user_registration';
+        return 'app_user_profile';
     }
 
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
 }
