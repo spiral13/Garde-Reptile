6ca730dc5a4a671d1caa44ab2ff25deec145fa17
LiliDevMart <lilidevmart@gmail.com>
2018-04-30 18:13:39 +0200

    Ajustement couleurs de base

--- src/AppBundle/Controller/AdController.php
+++ src/AppBundle/Controller/AdController.php
@@ -9,7 +9,7 @@
 namespace AppBundle\Controller;
 
 
-use AppBundle\Form\AdType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -111,5 +111,7 @@ class AdController extends Controller
         ]);
 
     }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
