8fa352472b2da6e72a8be6e3d1b7989711718826
LiliDevMart <lilidevmart@gmail.com>
2018-05-15 18:03:14 +0200

    Add no ad found

--- src/AppBundle/Controller/AdController.php
+++ src/AppBundle/Controller/AdController.php
@@ -15,6 +15,7 @@ use AppBundle\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface ;
 use Symfony\Component\Routing\Annotation\Route;
 
